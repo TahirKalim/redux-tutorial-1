1d375d59c659a1db4f9a4aa723e44a518f21df69
tahir kalim <thrkalim@gmail.com>
2021-04-20 21:47:56 +0200

    components container and service folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './User'
+import Home from './components/Home'
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-      {/* <h1>app component</h1> */}
-      <User data={{name:'Tahir Kalim', age: 26}}/>
+    <Home />
+    <Header />
     </div>
   );
 }
